a1749e1d5973215584844e213d09395bdcc30857
ianmorris13 <97080872+ianmorris13@users.noreply.github.com>
2022-03-15 23:02:01 -0700

    updateRepository

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,20 @@
 # to run this website and watch for changes: 
 # $ export FLASK_ENV=development; flask run
 
-
-from tracemalloc import start
-from flask import Flask, g, render_template, request
+from flask import Flask, render_template, request
 
 import pandas as pd
-import os
 from plotly import express as px
-import plotly.graph_objects as go
 
 flowersCA = pd.read_csv('./data/flowersCA.csv')
+averages  = pd.read_csv('./data/averages.csv')
 
 def flowersOverCA (startYear = 2000, endYear = 2020, nativeInfo = False, div = False):
-
+    
     flowers = flowersCA
     
     if div:
-        flowers = flowers[flowers['Div'] == div]
+        flowers = flowers[flowers['ClimateDivision'] == div]
 
     if nativeInfo:
         flowers = flowers[flowers['native'] == nativeInfo]
@@ -32,66 +29,89 @@ def flowersOverCA (startYear = 2000, endYear = 2020, nativeInfo = False, div = F
 
     return fig
 
-def avgFloweringByYear(startYear = 2000, endYear = 2020, nativeInfo = False, trendline = None, trendline_scope= False, div = False):
+def avgFloweringByYear (startYear = 2000, endYear = 2020, nativeInfo = False, trendline = None, trendline_scope= False, div = False):
     
     startYear = int(startYear)
     endYear = int(endYear)
+        
     ntv = ''
+    divNum = ''
     
     if div:
-        l = ['species', 'year', 'native', 'Div']
+        l = ['species', 'year', 'native', 'ClimateDivision']
         avg_df = flowersCA.groupby(l)["DOY"].mean().reset_index().round(0)
-        avg_df = avg_df[avg_df['Div'] == div]
+        avg_df = avg_df[avg_df['ClimateDivision'] == div]
+        divNum = f'0{div}'
     else:
         l = ['species', 'year', 'native']
         avg_df = flowersCA.groupby(l)["DOY"].mean().reset_index().round(0)
-
-    
+        
     if nativeInfo:
         avg_df = avg_df[avg_df['native'] == nativeInfo]
         if nativeInfo == 'yes':
             ntv = "Native "
         else:
             ntv = 'Non-Native '
-
-    if div:
-        if div == 'North':
-            div = "Northern "
-        elif div == 'Central':
-            div = 'Central '
-        else:
-            div = 'Southern '
+            
         
     avg_df = avg_df[avg_df['year'] >= startYear]
     avg_df = avg_df[avg_df['year'] <= endYear]
     
-    title = f"Average Flowering Day of {div}Californian {ntv}species from {startYear}-{endYear}"
-
+    title = f"Average {ntv}Flowering Day of Year in CA{divNum} from {startYear}-{endYear}"
+    
     if trendline_scope:
-        if trendline != 'ols':
-            fig = px.scatter(avg_df,
-                            x = "year",
-                            y = "DOY",
-                            color = 'species',
-                            trendline = trendline,
-                            title  = title
-                            )
-        else:
-            fig = px.scatter(avg_df,
-                            x = "year",
-                            y = "DOY",
-                            color = 'species',
-                            trendline = trendline,
-                            #trendline_scope = 'overall',
-                            title = title)            
+        fig =px.scatter(avg_df,
+                        x = "year",
+                        y = "DOY",
+                        color = 'species',
+                        trendline = trendline,
+                        trendline_scope = trendline_scope,
+                        title = title)
     else:
-        fig = px.scatter(avg_df,
-                         x = "year",
-                         y = "DOY",
-                         color = 'species',
-                         trendline = trendline,
-                         title = title)
+        fig =px.scatter(avg_df,
+                        x = "year",
+                        y = "DOY",
+                        color = 'species',
+                        trendline = trendline,
+                        title = title)
     return fig
+
+def divScatterTemp (climatediv = False):
+    
+    if climatediv:
+        avgDf = averages[averages['ClimateDivision'] == climatediv]
+        title = f'Average Temperature of CA0{climatediv} from 2000-2020'
+        
+    else:
+        avgDf = averages
+        title = 'Average Temperature of California from 2000-2020'
+        
+    fig =px.scatter(avgDf,
+                    x = "Year",
+                    y = "MeanAvgTemperature",
+                    trendline = "ols",
+                    color = 'AvgMonthlyPrecipitation',
+                    title = title)
+    return fig
+
+def divScatterPrecip (climatediv = False):
+    
+    if climatediv:
+        avgDf = averages[averages['ClimateDivision'] == climatediv]
+        title = f'Average Monthly Precipitation of CA0{climatediv} California from 2000-2020'
+        
+    else:
+        avgDf = averages
+        title = 'Average Monthly Precipitation of California from 2000-2020'
+        
+    fig = px.scatter(avgDf,
+                    x = "Year",
+                    y = "AvgMonthlyPrecipitation",
+                    trendline = "ols",
+                    color = 'MeanAvgTemperature',
+                    title = title)
+    return fig
+
 
 # Create web app, run with flask run
 # (set "FLASK_ENV" variable to "development" first!!!)
@@ -113,12 +133,15 @@ app = Flask(__name__)
 # http://localhost:5000
 
 def main():
-    return render_template('main_better.html')
+    return render_template('about.html')
 
 @app.route('/plantDistribution/', methods=['POST', 'GET'])
 def plantDistribution():
     if request.method == 'GET':
-        return render_template('plantDistribution.html')
+        flowersOverCA().write_html('./app/templates/distributionPlot.html')
+        start_year = 2000
+        end_year = 2000
+        return render_template('plantDistribution.html', startYear = start_year, endYear = end_year)
     else:
         plant_type = request.form.get('plant_type')
 
@@ -132,23 +155,35 @@ def plantDistribution():
         else:
             end_year = int(float(request.form.get('end_year')))
 
-        _div = request.form.get('_div')
+        if not request.form.get('_div'):
+            _div =  request.form.get('_div')
+        else:
+            _div =  int(float(request.form.get('_div')))
+
 
         flowersOverCA(startYear = start_year, endYear = end_year, nativeInfo = plant_type, div = _div).write_html('./app/templates/distributionPlot.html')
 
         if plant_type == 'yes':
             plant_type = 'native'
         elif plant_type == 'no':
-            plant_type = 'non-native'
+            plant_type = 'non-native'         
 
-        return render_template('plantDistribution.html', plot = True, startYear = start_year, endYear = end_year, nativeInfo = plant_type, div = _div)
+        return render_template('plantDistribution.html', startYear = start_year, endYear = end_year, nativeInfo = plant_type, div = _div)
 
 @app.route('/avgFlowering/', methods=['POST', 'GET'])
 def avgFlowering():
     if request.method == 'GET':
-        return render_template('avgFlowering.html')
+        avgFloweringByYear().write_html('./app/templates/avgDOYplot.html')
+        start_year = 2000
+        end_year = 2020
+
+        return render_template('avgFlowering.html', startYear = start_year, endYear = end_year)
 
     else:
+
+        plant_type = request.form.get('plant_type')
+        tline = request.form.get('tline')
+        scope = request.form.get('scope')
 
         if not request.form.get('start_year'):
             start_year = 2000
@@ -160,11 +195,10 @@ def avgFlowering():
         else:
             end_year = int(float(request.form.get('end_year')))
 
-        plant_type = request.form.get('plant_type')
-        _div = request.form.get('_div')
-        tline = request.form.get('tline')
-        scope = request.form.get('scope')
-
+        if not request.form.get('_div'):
+            _div =  request.form.get('_div')
+        else:
+            _div =  int(float(request.form.get('_div')))
 
         avgFloweringByYear(startYear = start_year, endYear = end_year, nativeInfo = plant_type, trendline = tline, trendline_scope= scope, div = _div).write_html('./app/templates/avgDOYplot.html')
 
@@ -173,8 +207,24 @@ def avgFlowering():
         elif plant_type == 'no':
             plant_type = 'non-native'
 
-        return render_template('avgFlowering.html', plot = True, startYear = start_year, endYear = end_year, nativeInfo = plant_type, div = _div)
+        return render_template('avgFlowering.html', startYear = start_year, endYear = end_year, nativeInfo = plant_type, div = _div)
 
-@app.route('/climate_plots/', methods=['GET'])
+@app.route('/climate_plots/', methods=['POST','GET'])
 def climate_plots():
+    if request.method == 'GET':
+        divScatterTemp().write_html('./app/templates/avgTempPlot.html')
+        divScatterPrecip().write_html('./app/templates/avgRainPlot.html')
+        return render_template('climate_plots')
+    else:
+        if not request.form.get('_div'):
+            _div =  request.form.get('_div')
+        else:
+            _div =  int(float(request.form.get('_div')))
+
+        divScatterTemp(_div).write_html('./app/templates/avgTempPlot.html')
+        divScatterPrecip(_div).write_html('./app/templates/avgRainPlot.html')    
     return render_template('climate_plots')
+
+@app.route('/species/', methods=['GET'])
+def species():
+    return render_template('species')    
