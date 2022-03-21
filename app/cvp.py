fea16d8cbfcb22fe1278987ca5badaf010075917
ianmorris13 <97080872+ianmorris13@users.noreply.github.com>
2022-03-20 20:54:45 -0700

    Update cvp.py

--- app/cvp.py
+++ app/cvp.py
@@ -274,4 +274,4 @@ def species():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug==False, host ="0.0.0.0", port=port)
+    app.run(host ="0.0.0.0", port=port)
